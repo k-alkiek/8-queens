18f80d79a1574f31fdfa06b8c167c860d1a70961
Khaled Alkiek <k.alkiek@gmail.com>
2019-12-13 13:40:04 +0200

    Modify gitignore

--- main.py
+++ main.py
@@ -5,20 +5,16 @@ import sys
 from file_io import read_config, write_config
 from chessboard.chessboard_state import ChessboardState
 
-
-
-
 if __name__ == '__main__':
     pass
     initial_config = read_config('input.txt')
 
     state = ChessboardState(initial_config)
     state.print_chessboard()
-
+    print(len(state.neighbors()));
 
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
     main.setWindowTitle("8 Queens")
     main.show()
     sys.exit(app.exec_())
-
