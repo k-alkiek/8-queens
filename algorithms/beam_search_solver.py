6dbe8f6f69a0e5cd2782e008105dba4f87d3ae64
Khaled Alkiek <k.alkiek@gmail.com>
2019-12-14 01:54:54 +0200

    Implemented beam search

--- algorithms/beam_search_solver.py
+++ algorithms/beam_search_solver.py
@@ -1,18 +1,50 @@
+import time
+from heapq import nsmallest
+
 from algorithms.abstract_solver import AbstractSolver
+from chessboard.chessboard_state import ChessboardState
+from chessboard.chessboard_state_node import ChessboardStateNode
 
 
 class BeamSearchSolver(AbstractSolver):
-    def solve(self, initial_chessboard_state):
-        pass
+    def __init__(self, k=1):
+        self.k = k
+        self.start_time = 0
+        self.end_time = 0
+        self.expanded_count = 0
+        self.depth = 0
+
+    def solve(self):
+        self.start_time = time.time()
+        # Initial random fringe with k states
+        fringe = [ChessboardStateNode(ChessboardState.random_state_one_per_col()) for _ in range(self.k)]
+        explored = set()
+
+        while fringe[0].cost() > 0:
+            self.depth += 1
+            new_fringe = []
+            for node in fringe:
+                if node in explored:
+                    continue
+
+                self.expanded_count += 1
+                explored.add(node)
+                for child in node.neighbors():
+                    if child not in explored:
+                        new_fringe.append(child)
+            fringe = nsmallest(self.k, new_fringe)
+
+        self.end_time = time.time()
+        return fringe[0].chessboard_state
 
     def get_running_time(self):
-        pass
+        return self.end_time - self.start_time
 
     def get_cost(self):
-        pass
+        return self.depth
 
     def get_expanded_count(self):
-        pass
+        return self.expanded_count
 
     def get_best_k(self):
         pass
